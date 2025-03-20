b82348e7b391c2f6ed35b912f6c9e3545547313d
Aearsears <53102185+Aearsears@users.noreply.github.com>
2025-03-20 15:38:40 -0400

    nuke and start over

--- src/network/config.js
+++
@@ -1,6 +0,0 @@
-export const NETWORK_CONFIG = {
-    SERVER_URL: 'ws://localhost:8080',
-    POSITION_UPDATE_RATE: 50, // ms
-    INTERPOLATION_DELAY: 100, // ms
-    MAX_EXTRAPOLATION_TIME: 1000 // ms
-};
